20ebb756fed5da7b1a16c65a25eaf0dbaae3c18e
Aya Youssef <121063569+Aya190@users.noreply.github.com>
2024-11-10 19:16:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+ 
+ //let num = window.prompt();
+//console.log( 0.025 * num + ' جنية ' );
+let x =100;
+//console.log(String(x));
+
+//let name = 'i love java script';
+//console.log(name.indexOf('o'));
+//console.log(name.split(''));
+//console.log(name.split('',3));
+//let names = ['ahmed' , 'youssef' ,'hessen']
+//names.splice(0,1);
+//console.log(names);
+/*let arr1 = ['ahmed','hessen','mohamed','omar','zyad'];
+let arr2 =['aya','zeniab','nour'];
+console.log(arr1.sort()); //sort the array
+console.log(arr1.concat(arr2)); // دمج 2 arrays
+console.log(arr1.join('-')); //تحويل الarray to string*/
+let askEmail = prompt('Enter your Email');
+let email = 'aya@gmail.com';
+console.log(askEmail == email);
+
+ 
